919eacf4ffb7200ea7ccbe734856b9ce539ff739
Conor Dowling <conor@Noras-iMac.home>
2016-04-21 18:04:15 -0400

    server skeleton code

---
+++ server/query_cards.py
@@ -0,0 +1,27 @@
+import unirest
+import json
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+
+db = client.hearthstone
+card_collection = db.cards
+
+def get_cards_from_api():
+	response = unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1",
+  		headers={
+    		"X-Mashape-Key": "Kv2act4lSmmshliZ7JasXPYR6wiop1ZKZgzjsn4OcmVrAaGvsJ"
+  		}	
+	)
+
+	card_collection.drop()
+
+	for s, cards in response.body.items():
+		for card in cards:
+			if card["type"] in ["Minion", "Spell", "Weapon"]:
+				card_collection.insert(card)
+
+
+	db.cards.create_index([('cardId', pymongo.ASCENDING)], unique=True)
+
+get_cards_from_api()
---
+++ server/server.py
@@ -0,0 +1,23 @@
+import django
+
+
+
+#Server Functionality
+
+def create_lobby():
+
+
+def join_lobby():
+
+
+def get_lobbies():
+
+
+def start_draft():
+
+
+def pick_card():
+
+
+def send_chat():
+	
